921769b55e0a05146f0e356bfbba5100212cc37f
highslater <highslater@hotmail.com>
2019-03-27 15:55:41 -0400

    commit_103

---
+++ 103_Send_Emails.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.7
+
+"""103_Send_Emails.py.
+
+One Hundred and Third Program of the Corey Schafer Python Series.
+
+"""
+import logging
+from platform import python_version
+from sys import hexversion
+from datetime import datetime as dt
+
+NOW = dt.today()
+PRINT_VERSION_INFO = True
+PRINT_TIME = True
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+version_info = "The Python Version is: {}  #{}".format(
+    python_version(), str(hexversion))
+logging.basicConfig(filename="LOG_files/LOG_103.Log",
+                    level=logging.DEBUG, format=LOG_FORMAT,
+                    filemode='w')
+logger = logging.getLogger()
+logger.info(version_info) if PRINT_VERSION_INFO else None
+logger.info(NOW.strftime("%A, %B, %d, %Y")) if PRINT_TIME else None
+logger.info("103_Send_Emails.py RUN / START")
+
+print("Today is:", NOW.strftime("%A, %B, %d, %Y")) if PRINT_TIME else None
+print(version_info) if PRINT_VERSION_INFO else None
