881f01264a581fdb20156c408cc3d1a3c611eef4
highslater <highslater@hotmail.com>
2019-04-02 15:19:05 -0400

    commit_007

--- 007_Loops_and_Iterations.py
+++ 007_Loops_and_Iterations.py
@@ -31,3 +31,62 @@ logger.info("007_Loops_and_Iterations.py RUN / START")
 
 print("Today is:", NOW.strftime("%A, %B, %d, %Y")) if PRINT_TIME else None
 print(version_info) if PRINT_VERSION_INFO else None
+
+nums = [1, 2, 3, 4, 5]
+
+print('', "-" * 88)
+for num in nums:
+    print('', num)
+print('', "-" * 88)
+
+print('', "-" * 88)
+for num in nums:
+    if num == 3:
+        print('', 'Found It.')
+        break
+    print('', num)
+print('', "-" * 88)
+
+print('', "-" * 88)
+for num in nums:
+    if num == 3:
+        print('', 'Found It.')
+        continue
+    print('', num)
+print('', "-" * 88)
+
+print('', "-" * 88)
+for num in nums:
+    for letter in "abc":
+        print('', num, letter)
+print('', "-" * 88)
+
+print('', "-" * 88)
+for i in range(1, 11):
+    print('', i)
+print('', "-" * 88)
+
+print('', "-" * 88)
+x = 0
+while x < 10:
+    print('', x)
+    x += 1
+print('', "-" * 88)
+
+print('', "-" * 88)
+x = 0
+while x < 10:
+    if x == 5:
+        break
+    print('', x)
+    x += 1
+print('', "-" * 88)
+
+print('', "-" * 88)
+x = 0
+while True:
+    if x == 5:
+        break
+    print('', x)
+    x += 1
+print('', "-" * 88)
